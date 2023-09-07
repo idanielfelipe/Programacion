ff071c2bf3d6e13d5535c6a14ce42e330c3df738
idanielfelipe <142370105+idanielfelipe@users.noreply.github.com>
2023-09-06 23:26:09 -0500

    Update kilogramos a libras.py
    tenia eror en la coonversion

--- Tareas/kilogramos a libras.py
+++ Tareas/kilogramos a libras.py
@@ -3,6 +3,6 @@ kg=(int(input("ingrese una cantidad en kilogramos ")))
 while kg<0:
         kg=(int(input(print("numero erroneo, inserte una cantidad correcta "))))
 else:
-    lib=kg*1000
+    lib=kg*2
     print("la cantidad de libras es: ", lib)
 
